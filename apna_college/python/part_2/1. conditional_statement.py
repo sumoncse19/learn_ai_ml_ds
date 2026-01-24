cd72f5ec265907516895c966ab13a3eca879e093
sumoncse19 <mdsumoncse19@gmail.com>
2026-01-25 01:03:22 +0600

    Add Python fundamentals assignment with conditional statements

---
+++ apna_college/python/part_2/1. conditional_statement.py
@@ -0,0 +1,9 @@
+# if, elif, else statements in Python
+
+age = 18
+if age >= 18:
+    print("You are an adult.")
+elif age >= 13:
+    print("You are a teenager.")
+else:
+    print("You are a child.")
